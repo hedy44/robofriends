bf623c56f561519ea896e6455e565071edc8082d
Hernani Diniz <hdiniz4@gmail.com>
2023-09-06 01:46:38 +0100

    chore: code cleaning

--- src/containers/App.js
+++ src/containers/App.js
@@ -31,15 +31,14 @@ class App extends Component{
     }
 
     render(){
-
-        const filteredRobots= this.state.robots.filter(robots=>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const { robots, searchfield} = this.state;
+        const filteredRobots= robots.filter(robot=>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
 
-        if(this.state.robots.length === 0){
-            return <h1 className='tc f1'>Loading</h1>
-        }else{
-            return(
+        return !robots.length ?
+           <h1 className='tc f1'>Loading</h1> :
+        (
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
@@ -53,7 +52,6 @@ class App extends Component{
         }
     
     }
-    
-}
+
 
 export default App;
